4f15fdce868d91b537e7856c5d6e17662bb9bdf9
adamp013 <ado.palko@outlook.com>
2026-03-26 18:45:18 +0100

    zabudnuty commit

--- hono-client/src/test.vue
+++ hono-client/src/test.vue
@@ -5,7 +5,7 @@ import { ref } from 'vue'
 </script>
 
 <template>
-    <div class="min-h-screen bg-[#faf3e0] flex ">
+    <div class="min-h-screen bg-[#fbf6e8] flex ">
         <!-- <div class="min-w-[240px] min-h-screen bg-[#eadfcc]">
             <div class="h-full flex justify-center">
                 <div class="flex-1 justify-center" >
@@ -27,15 +27,15 @@ import { ref } from 'vue'
             <!-- Cast navigacie-->
             <div class="flex justify-around items-end mb-6 w-full px-2">
                 <div class="flex flex-col items-center gap-1 cursor-pointer group">
-                    <span class="text-2xl">🏠</span>
+                    <span class="text-2xl hover:scale-110 transition-transform duration-200">🏠</span>
                     <span class="text-[10px] uppercase font-bold text-text opacity-60 group-hover:opacity-100">Home</span>
                 </div>
                 <div class="flex flex-col items-center gap-1 cursor-pointer group">
-                    <span class="text-2xl">📝</span>
+                    <span class="text-2xl hover:scale-110 transition-transform duration-200">📝</span>
                     <span class="text-[10px] uppercase font-bold text-text opacity-60 group-hover:opacity-100">New</span>
                 </div>
                 <div class="flex flex-col items-center gap-1 cursor-pointer group">
-                    <span class="text-2xl">📌</span>
+                    <span class="text-2xl hover:scale-110 transition-transform duration-200">📌</span>
                     <span class="text-[10px] uppercase font-bold text-text opacity-60 group-hover:opacity-100">Pins</span>
                 </div>
             </div>
@@ -43,26 +43,31 @@ import { ref } from 'vue'
             <hr class="border-2 border-secondary opacity-20 mb-10 mx-2" />
 
             <!-- Cast Playlistov-->
-            <div class="flex flex-col gap-10 overflow-y-auto items-center w-full">
-                <div class="flex flex-col items-center">
-                    <div class="w-[120px] h-[120px] bg-secondary rounded-xl cursor-pointer"></div>
+            <div class="flex flex-col gap-10 overflow-y-auto items-center w-full p-4">
+                <div class="flex flex-col items-center w-[140px]">
+                    <div class="w-[120px] h-[120px] bg-secondary rounded-xl cursor-pointer hover:shadow-2xl transition-all hover:scale-105 duration-300"></div>
                     <span class="text-[13px] mt-3 text-secondary">Playlist 1</span>
                 </div>
 
-                <div class="flex flex-col items-center">
-                    <div class="w-[120px] h-[120px] bg-[var(--color-primary)] rounded-xl cursor-pointer"></div>
-                    <span class="text-[13px] mt-3 font-bold text-[var(--color-accent)]">Playlist 2 (aktivny)</span>
+                <div class="flex flex-col items-center w-[140px]">
+                    <div class="w-[120px] h-[120px] bg-[var(--color-primary)] rounded-xl cursor-pointer hover:shadow-2xl transition-all hover:scale-105 duration-300"></div>
+                    <span class="text-[13px] mt-3 font-bold text-[var(--color-accent)] leading-tight">Playlist 2 (aktivny)</span>
                 </div>
 
-                <div class="flex flex-col items-center">
-                    <div class="w-[120px] h-[120px] bg-secondary rounded-xl cursor-pointer"></div>
+                <div class="flex flex-col items-center w-[140px]">
+                    <div class="w-[120px] h-[120px] bg-secondary rounded-xl cursor-pointer hover:shadow-2xl transition-all hover:scale-105 duration-300"></div>
                     <span class="text-[13px] mt-3 text-secondary">Playlist 3</span>
                 </div>
             </div>
+        </aside>
 
-            <!--Cast Header-->
-            <div class="">
-
+        <!--Cast Header-->
+        <aside class=" min-w-screen h-[100px] bg-[#faf3e0] flex flex-row items-center py-8 px-4 border-r shadow-lg">
+            
+            <div class="w-[440px] h-[52px] bg-[#ffffff] rounded-4xl cursor-pointer hover:shadow-2xl transition-all border border-secondary"></div>
+            
+            <div class="flex-row justify-items-end">
+                <hr class="border-2 border-secondary opacity-20 mb-10 mx-2" />
             </div>
         </aside>
     </div>
---
+++ reports/reports/frontend.md
@@ -0,0 +1,3 @@
+Zabudol som minuly tyzden dat commit.
+
+Pracoval som na frontende, zatial spraveny sidebar a rozrobeny header.
